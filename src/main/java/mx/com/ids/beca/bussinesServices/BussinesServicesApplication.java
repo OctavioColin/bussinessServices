0e10df41696bb29d57f995571257f3def8aafa46
octavo.colin <octavio.colin@ids.com.mx>
2022-07-04 16:44:04 -0500

    segundo commit

---
+++ src/main/java/mx/com/ids/beca/bussinesServices/BussinesServicesApplication.java
@@ -0,0 +1,13 @@
+package mx.com.ids.beca.bussinesServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussinesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussinesServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/ids/beca/bussinesServices/BussinesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.ids.beca.bussinesServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussinesServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
