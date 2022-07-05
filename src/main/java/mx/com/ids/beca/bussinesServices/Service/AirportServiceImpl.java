ae558521fd1300b579a594a789ff211c29f5af8f
octavo.colin <octavio.colin@ids.com.mx>
2022-07-04 20:28:23 -0500

    Cuarto comit, se agrega anotacion @Service a AirportServiceImpl, se agrega la dependencia starter-web, se agrega la anotacion ddl en application.properties.

--- src/main/java/mx/com/ids/beca/bussinesServices/Service/AirportServiceImpl.java
+++ src/main/java/mx/com/ids/beca/bussinesServices/Service/AirportServiceImpl.java
@@ -3,10 +3,12 @@ package mx.com.ids.beca.bussinesServices.Service;
 import mx.com.ids.beca.bussinesServices.Model.Airport;
 import mx.com.ids.beca.bussinesServices.Repository.AirportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AirportServiceImpl implements AirportService{
 
     @Autowired
